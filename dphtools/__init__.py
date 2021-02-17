d570f29ad3da35106e7996807243da214add802b
David Hoffman <dave.p.hoffman@gmail.com>
2021-02-17 14:15:13 -0800

    basic structure set up

---
+++ dphtools/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __init__.py
+"""
+Tools for optics and image analysis
+
+Copyright (c) 2021, David Hoffman
+"""
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
