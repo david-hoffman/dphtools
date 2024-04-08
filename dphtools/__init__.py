ea77fe2cbd86163aff7efd7640a47a7a7beae0fc
David Hoffman <dave.p.hoffman@gmail.com>
2024-04-08 13:35:28 -0700

    update versioneer

--- dphtools/__init__.py
+++ dphtools/__init__.py
@@ -7,11 +7,6 @@ Tools for optics and image analysis.
 Copyright (c) 2021, David Hoffman
 """
 
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
 
 from . import _version
-
-__version__ = _version.get_versions()["version"]
+__version__ = _version.get_versions()['version']
